389ac7f26f271989f03d018444a44c3dd72952e5
Rohit Chaudhry <rohitchaudhry362@gmail.com>
2021-09-01 13:14:55 -0400

    Added CRUD operations for Genres with MongoDb

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const Joi = require('joi');
 const genres = require('./routes/genres');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/vidly')
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.error("Could not connect to mongodb.. ", err));
 
 app.use(express.json());
 app.use('/api/genres', genres);
